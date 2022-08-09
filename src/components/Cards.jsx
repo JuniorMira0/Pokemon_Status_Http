5ff2a876d3e8a3d5287ee171ccdc60f4b9bd2439
junior <j-unyor14@hotmail.com>
2022-08-08 23:24:01 -0300

    change background cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -51,11 +51,11 @@ const Cards = () => {
             <ul>
                   <li>
                     <a className="card">
-                      <img src="https://i.imgur.com/oYiTqum.jpg" className="card__image" alt="" />
+                      <img src={`https://cdn.traction.one/pokedex/pokemon/${code.id}.png`} className="card__image" alt="" />
                       <div className="card__overlay">
                         <div className="card__header">
                           <svg className="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
-                          <img className="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+                          <img className="card__thumb" src={`https://cdn.traction.one/pokedex/pokemon/${code.id}.png`} alt="" />
                           <div className="card__header-text">
                             <h3 className="card__title">{code.title}</h3>
                           </div>
